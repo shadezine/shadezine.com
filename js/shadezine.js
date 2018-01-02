329f5abcba1d25aed3a47d16de5d6d24b0e95b88
omarmhmmd <omar.mhmmd@gmail.com>
2018-01-01 19:02:59 -0800

    top and bottom marquees

--- js/shadezine.js
+++ js/shadezine.js
@@ -1,6 +1,34 @@
+var duration = 20000;
+var gap = 200;
+var delay = -20000;
+
 $(function () {
     $('.marqueeTop').marquee({
-        duration: 20000
+      duration: duration,
+      //time in milliseconds before the marquee will start animating
+      delayBeforeStart: delay,
+      //'left' or 'right'
+      direction: 'left',
+      //true or false - should the marquee be duplicated to show an effect of continues flow
+      duplicated: true,
+
+      pauseOnHover: true,
+
+      gap: gap
+    });
+
+    $('.marqueeBottom').marquee({
+      duration: duration,
+      //time in milliseconds before the marquee will start animating
+      delayBeforeStart: delay,
+      //'left' or 'right'
+      direction: 'right',
+      //true or false - should the marquee be duplicated to show an effect of continues flow
+      duplicated: true,
+
+      pauseOnHover: true,
+
+      gap: gap
     });
 
     // $('.marqueeLeft').marquee({
