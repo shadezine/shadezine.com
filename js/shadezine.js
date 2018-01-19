223c0a7b0a04c14df07db4bcf5743ae3c90714fe
omarmhmmd <omar.mhmmd@gmail.com>
2018-01-19 12:19:41 -0800

    navigation 001

--- js/shadezine.js
+++ js/shadezine.js
@@ -3,6 +3,7 @@ var gap = 200;
 var delay = -20000;
 
 $(function () {
+  var clicked = false;
   /******** MARQUEE ********/
   $('.marqueeTop').marquee({
     duration: duration,
@@ -33,6 +34,17 @@ $(function () {
   });
   /******** END MARQUEE ********/
 
+  /******** GIRLZ ********/
+  var randomGirlz=[];
+  $(".girlz").each(function(){ randomGirlz.push($(this));});
+
+  for (var i = 0; i < randomGirlz.length; i++) {
+    randLocGirlz();
+    $(randomGirlz[i]).css('left', randPosX);
+    $(randomGirlz[i]).css('top', randPosY);
+  }
+  /******** END GIRLZ ********/
+
   /******** BOUNCE ********/
   var bodyWidth =  window.innerWidth;
   var bodyHeight =  window.innerHeight;
@@ -61,55 +73,51 @@ $(function () {
       inc:1
     });
   }
-
-  // $('.bounceAbout').DVDBounce({
-  //   inc:1
-  // });
-  //
-  // $('.bounceTeam').DVDBounce({
-  //   inc:1
-  // });
-  //
-  // $('.bounceShop').DVDBounce({
-  //   inc:1
-  // });
-  //
-  // $('.bounceTV').DVDBounce({
-  //   inc:1
-  // });
-  //
-  // $('.bounceInternet').DVDBounce({
-  //   inc:1
-  // });
-  //
-  // $('.bounceProjects').DVDBounce({
-  //   inc:1
-  // });
   /******** END BOUNCE ********/
+
+  /******** HOME NAV ********/
+  $(".banners").click(function(){
+    clicked = false;
+    $(".bouncing").css("visibility", "visible");
+    $(".st0").css("visibility", "visible");
+    $("body").css("background-color", "#fff");
+    $(".banner").css("background", "#fff");
+  });
+  /******** END HOME NAV ********/
+
 
   /******** MENU ********/
   $(".bounceAbout").mouseover(function(){
     $(".banner").css("background", "red");
     $(".bannerLeft").html('<span class = "firstTicker">ABOUT</span> ABOUT ABOUT ABOUT ABOUT ABOUT ');
     $(".bannerRight").html('<span class = "firstTicker">ABOUT</span> ABOUT ABOUT ABOUT ABOUT ABOUT ');
+    $(".st0").css("fill", "red");
   });
 
   $(".bounceAbout").mouseout(function(){
-    $(".banner").css("background", "white");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    if (!clicked) {
+      $(".banner").css("background", "white");
+    }
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".st0").css("fill", "red");
   });
 
   $(".bounceTeam").mouseover(function(){
     $(".banner").css("background", "#FF00FF");
     $(".bannerLeft").html('<span class = "firstTicker">TEAM</span> TEAM TEAM TEAM TEAM TEAM ');
     $(".bannerRight").html('<span class = "firstTicker">TEAM</span> TEAM TEAM TEAM TEAM TEAM ');
+    $(".st0").css("fill", "#FF00FF");
+
   });
 
   $(".bounceTeam").mouseout(function(){
-    $(".banner").css("background", "white");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    if (!clicked) {
+      $(".banner").css("background", "white");
+    }
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".st0").css("fill", "red");
   });
 
   $(".bounceShop").mouseover(function(){
@@ -122,8 +130,8 @@ $(function () {
   $(".bounceShop").mouseout(function(){
     $(".banner").css("background", "white");
     $(".banner").css("color", "#000");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
   });
 
   $(".bounceTV").mouseover(function(){
@@ -131,13 +139,15 @@ $(function () {
     $(".banner").css("color", "#FFF");
     $(".bannerLeft").html('<span class = "firstTicker">SHADETV</span> SHADETV SHADETV SHADETV SHADETV SHADETV SHADETV');
     $(".bannerRight").html('<span class = "firstTicker">SHADETV</span> SHADETV SHADETV SHADETV SHADETV SHADETV SHADETV');
+    $(".st0").css("fill", "#0000FF");
   });
 
   $(".bounceTV").mouseout(function(){
     $(".banner").css("background", "white");
     $(".banner").css("color", "#000");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".st0").css("fill", "#FF0000");
   });
 
   $(".bounceInternet").mouseover(function(){
@@ -145,36 +155,49 @@ $(function () {
     $(".banner").css("color", "#00ff00");
     $(".bannerLeft").html('<span class = "firstTicker">INTERNET</span> INTERNET INTERNET INTERNET INTERNET INTERNET');
     $(".bannerRight").html('<span class = "firstTicker">INTERNET</span> INTERNET INTERNET INTERNET INTERNET INTERNET');
+    $(".st0").css("fill", "#00FF00");
   });
 
   $(".bounceInternet").mouseout(function(){
     $(".banner").css("background", "#fff");
     $(".banner").css("color", "#000");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".st0").css("fill", "#ff0000");
   });
 
   $(".bounceProjects").mouseover(function(){
     $(".banner").css("background", "#FFFF00");
     $(".bannerLeft").html('<span class = "firstTicker">PROJECTS</span> PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS');
     $(".bannerRight").html('<span class = "firstTicker">PROJECTS</span> PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS');
+    $(".st0").css("fill", "#ffff00");
   });
 
   $(".bounceProjects").mouseout(function(){
     $(".banner").css("background", "#fff");
-    $(".bannerRight").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
-    $(".bannerLeft").html('<span class = "firstTicker">SHADEZINE</span> SHADEZINE SHADEZINE SHADEZINE SHADEZINE SHADEZINE ');
+    $(".bannerRight").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".bannerLeft").html('<span class = "firstTicker">SHADE</span> SHADE SHADE SHADE SHADE SHADE ');
+    $(".st0").css("fill", "#ff0000");
   });
   /******** END MENU ********/
 
-  /******** GIRLZ ********/
-  var randomGirlz=[];
-  $(".girlz").each(function(){ randomGirlz.push($(this));});
+  /******** CLICKS ********/
+  $(".bounceAbout").click(function(){
+    clicked = true;
+    $(".bouncing").css("visibility", "hidden");
+    $(".st0").css("visibility", "hidden");
+    $("body").css("background-color", "#ff0000");
+    $(".banner").css("background", "#FF0000");
+    // $('.banner').attr('style', 'background: #ff0000 !important');
+  });
 
-  for (var i = 0; i < randomGirlz.length; i++) {
-    randLocGirlz();
-    $(randomGirlz[i]).css('left', randPosX);
-    $(randomGirlz[i]).css('top', randPosY);
-  }
-
+  $(".bounceTeam").click(function(){
+    clicked = true;
+    $(".bouncing").css("visibility", "hidden");
+    $(".st0").css("visibility", "hidden");
+    $("body").css("background-color", "#FF00FF");
+    $(".banner").css("background", "##FF00FF");
+    // $('.banner').attr('style', 'background: #ff0000 !important');
+  });
+  /******** CLICKS ********/
 });
