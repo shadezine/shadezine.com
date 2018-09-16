fc8546e774b52ab193b2e1d8092b01a920704f61
shadezine <32992830+shadezine@users.noreply.github.com>
2018-09-16 11:39:46 -0700

    update channel 1
    update channel 1

--- js/shadezine.js
+++ js/shadezine.js
@@ -431,7 +431,7 @@ $(function () {
       $(".TVCopy").css("visibility", "hidden");
       $("iframe").css("visibility", "visible");
       $("#backgroundTV").attr('src', 'images/bg.png');
-      $("iframe").attr('src', 'https://www.youtube.com/embed/w8x7tPK3T1c');
+      $("iframe").attr('src', 'https://www.youtube.com/watch?v=8hoDcsMMthE&t=1s');
       $("#backgroundTV").css("visibility", "visible");
 
     });
